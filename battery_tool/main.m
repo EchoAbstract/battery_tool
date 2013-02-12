430c5e5c91a57676d59d4b8576b3aafda50d1911
Brian Wilson <brian@tokyo.local>
2013-02-11 21:04:28 -0500

    Initial Commit

---
+++ battery_tool/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  battery_tool
+//
+//  Created by Brian Wilson on 2/11/13.
+//  Copyright (c) 2013 Polytopes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
